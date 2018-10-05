ab7b4a0ac3187c235be4c8a7fc3388e60fcd2447
EricaMartinada <martinadaerica11500@gmail.com>
2018-10-05 18:44:32 +0800

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, World")
+print("Hello, World"); 
+print("Hello", "World"); 
+print("Hello, World", sep =":", end="$") 
+print(" ericamaganda"); 
+print(" ron araon gurangon"); 
+print("RON ARAON GURANGON"); 

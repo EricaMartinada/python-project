e89d500b4f7431d69fa06abd1cb7b7fc8b3b539b
EricaMartinada <martinadaerica11500@gmail.com>
2018-10-05 19:03:15 +0800

    git

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print("Hello, World"); 
 print("Hello", "World"); 
 print("Hello, World", sep =":", end="$") 
-print(" IKAY KAPAYON SAYS HELLO TO: "); 
+print(" IKAY KAPAYON SAYS THE PROPER USE OF TOOTHBRUSH TO: "); 
 print("RON ARAON GURANGON"); 
 print("{}{}".format("ADNU",2018)); 

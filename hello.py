f00a90c976f8f83d807b9354420f4ea1e8c9d2cc
EricaMartinada <martinadaerica11500@gmail.com>
2018-10-05 18:53:43 +0800

    git

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print("Hello, World"); 
 print("Hello", "World"); 
 print("Hello, World", sep =":", end="$") 
-print(" ericamaganda"); 
-print(" ron araon gurangon"); 
+print(" IKAY KAPAYON SAYS HELLO TO: "); 
 print("RON ARAON GURANGON"); 
+print("{}{}".format("ADNU",2018)); 

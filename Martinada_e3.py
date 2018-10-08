68c6939caee353f8d78157cec5139a5d3b85e697
EricaMartinada <martinadaerica11500@gmail.com>
2018-10-08 18:12:47 +0800

    git

--- Martinada_e3.py
+++ Martinada_e3.py
@@ -4,3 +4,10 @@
 #                that computes and displays
 #                the sum of each number squared
 #               
+data = input ("Enter a comma separated list of numbers")
+
+#Compute sum of squares
+total = sum ([float (k) **2 for k in data.split(",")])
+
+#Print Result
+print ("Sum of squares: {:,2f}".format(total))

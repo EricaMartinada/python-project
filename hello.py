47a1639f78e153811876260c80ea011d14a04a6e
EricaMartinada <martinadaerica11500@gmail.com>
2018-10-08 18:01:08 +0800

    Add

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ print("Hello, World", sep =":", end="$")
 print(" IKAY KAPAYON SAYS THE PROPER USE OF TOOTHBRUSH TO: "); 
 print("RON ARAON GURANGON"); 
 print("{}{}".format("ADNU",2018)); 
+age= 17
+weight= 43
+print  (type (age))
+print (type(weight))
+data = {"name": "Erica",
+         "age": 17,
+         "height": 150,
+         "fave_fruits": ["mangoes"]}
+print ("My name is {}". format (data ["name"]))
